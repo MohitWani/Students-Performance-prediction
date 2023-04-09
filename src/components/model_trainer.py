1995709ac53a7fd31e88c2eb316c712dd5964b85
Mohit Wani <wanimohit1@gmail.com>
2023-04-09 11:38:39 +0530

    model trianer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+from exception import CustomException
+from logger import logging
+
+from utils import save_object,evaluate_models
+
+@dataclass()
+class ModelTrainerConfig:
+    train_model_path = os.path.join('artifacts', 'Model.pkl')
+
+class ModelTrainer:
+    def __int__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            logging.info("Split Data and Start trianing models")
+            X_train, y_train, X_test, y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,1]
+            )
+
+            models = {'random_forest':RandomForestRegressor, 'adaboost':AdaBoostRegressor, 'gradientboost':GradientBoostingRegressor,
+                      'xgboost':XGBRegressor, 'linear_reg':LinearRegression, 'knn':KNeighborsRegressor, 'decisiontree':DecisionTreeRegressor}
+
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                                 models=models)
+
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path= self.model_trainer_config.train_model_path,
+                obj= best_model
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
